cc46aea1e70956fcc7f5ae8e0a1b37bbd82743c1
Jill Thomas <jmthomas8111@gmail.com>
2022-07-19 21:00:47 -0400

    pwd and ls

--- bash.js
+++ bash.js
@@ -1,11 +1,14 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls');
 //Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the newline
-  if(cmd === 'pwd'){
-    process.stdout.write(process.cwd());
+  if (cmd === 'pwd') {
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   }
-  process.stdout.write('\nprompt > ');
 });
