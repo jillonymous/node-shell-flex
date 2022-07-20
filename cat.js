a06b7f95c034810b7fc33653cdbf77903297e187
Anisah Moise <anisahleila@hotmail.com>
2022-07-20 01:26:14 +0000

    cat file

---
+++ cat.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+module.exports = function cat (file) {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      process.stdout.write(data.trim().toString());
+      process.stdout.write('\nprompt > ');
+    }
+  })
+}
